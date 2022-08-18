b51ecef183a2bc430c551bdf72392e36dece541a
IgnacioIraola <iraolai@hotmail.com>
2022-08-18 19:39:55 -0300

    inicio carrito de compra

---
+++ app.js
@@ -0,0 +1,21 @@
+class Producto{
+    constructor(bebida,precio){
+        this.bebida = bebida;
+        this.precio = Number(precio);
+    }
+}
+
+let compra[]
+
+for (let i = 0; i <=2; i++) {
+    let bebida = prompt ("Ingrese la bebida deseada: ")
+    let precio = prompt ("Ingrese el valor de la bebida: ")
+
+    let producto = new Producto(bebida,precio)
+    compra.push(producto)
+}
+
+const precioBebida = compra.map(dato => dato.precio)
+
+let precioTotal = precioBebida.reduce((a,b) =>a+b)
+alert("Precio total: " + precioTotal)
